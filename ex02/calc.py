130d682457936fbf2b8b93cd556b94474c1e7d2f
C0B21139_fuse_ryota <c0b2113943@edu.teu.ac.jp>
2022-10-04 18:11:33 +0900

    修正しました #8

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,15 +5,15 @@ import tkinter.messagebox as tkm
 
 
 
-def click(event):  #練習３
-    btn = event.widget
-    num = int(btn["text"])
-    #tkm.showinfo(f"{num}", f"「{num}のボタンが押されました」")
-    entry.insert(tk.END, num) #5
+# def click(event):  #練習３
+#     btn = event.widget
+#     num = int(btn["text"])
+#     #tkm.showinfo(f"{num}", f"「{num}のボタンが押されました」")
+#     entry.insert(tk.END, num) #5
 
 def click2(event):  #6
     btn = event.widget
-    num = str(btn["text"])
+    num = (btn["text"])
     entry.insert(tk.END, num) 
 
 def click_eq(event):  #7
@@ -52,7 +52,7 @@ entry.grid(row=0, column=0, columnspan=4)
 r, c = 1, 0
 for i, num in enumerate(range(9,-1, -1), 1):  #練習２
     btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
-    btn.bind("<1>", click)
+    btn.bind("<1>", click2)
     btn.grid(row=r, column=c)
     c += 1
     if i%3 == 0:
