b877d33ab9ff51d1488f8ce06a6764156a5d119d
C0B21139_fuse_ryota <c0b2113943@edu.teu.ac.jp>
2022-10-04 14:48:24 +0900

    練習４

--- ex02/calc.py
+++ ex02/calc.py
@@ -4,16 +4,20 @@ import tkinter.messagebox as tkm
 
 
 
-def click(event):
+def click(event):  #練習３
     btn = event.widget
     num = int(btn["text"])
     tkm.showinfo(f"{num}", f"「{num}のボタンが押されました」")
 
-root =tk.Tk()
+root =tk.Tk()               #練習１
 root.geometry("300x500")
-r=0
-c=0
-for i, num in enumerate(range(9,-1, -1), 1):
+
+entry=tk.Entry(root,width=10,font=("Times New Roman",40),justify="right")#練習４
+entry.grid(row=0, column=0, columnspan=3)
+
+
+r, c = 1, 0
+for i, num in enumerate(range(9,-1, -1), 1):  #練習２
     btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
     btn.bind("<1>", click)
     btn.grid(row=r, column=c)
