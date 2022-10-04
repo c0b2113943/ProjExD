7f5f43b13b1c7511836ddb4198826980dc565ce1
C0B21139_fuse_ryota <c0b2113943@edu.teu.ac.jp>
2022-10-04 16:02:12 +0900

    四則演算

--- ex02/calc.py
+++ ex02/calc.py
@@ -18,16 +18,18 @@ def click2(event):  #6
 
 def click_eq(event):  #7
     get=entry.get()
-    ans=eval(get)
+    get=get.replace("×","*")   #掛け算、割り算対応
+    get=get.replace("÷","/")
+    ans=round(eval(get),10)
     entry.delete(0,tk.END)
     entry.insert(tk.END, ans)
 
 
 root =tk.Tk()               #練習１
-root.geometry("300x500")
+root.geometry("400x600")
 
-entry=tk.Entry(root,width=10,font=("Times New Roman",40),justify="right")#練習４
-entry.grid(row=0, column=0, columnspan=3)
+entry=tk.Entry(root,width=14,font=("Times New Roman",40),justify="right")#練習４
+entry.grid(row=0, column=0, columnspan=4)
 
 
 r, c = 1, 0
@@ -42,7 +44,21 @@ for i, num in enumerate(range(9,-1, -1), 1):  #練習２
 
 btn = tk.Button(root, text="+", font=("", 30), width=4, height=2)#6
 btn.bind("<1>", click2)
+btn.grid(row=4, column=3)
+btn = tk.Button(root, text="-", font=("", 30), width=4, height=2)#引き算
+btn.bind("<1>", click2)
+btn.grid(row=3, column=3)
+btn = tk.Button(root, text="×", font=("", 30), width=4, height=2)#掛け算
+btn.bind("<1>", click2)
+btn.grid(row=1, column=3)
+btn = tk.Button(root, text="÷", font=("", 30), width=4, height=2)#割り算
+btn.bind("<1>", click2)
+btn.grid(row=2, column=3)
+btn = tk.Button(root, text=".", font=("", 30), width=4, height=2)#小数
+btn.bind("<1>", click2)
 btn.grid(row=4, column=1)
+
+
 
 btn = tk.Button(root, text="=", font=("", 30), width=4, height=2)#7
 btn.bind("<1>", click_eq)
