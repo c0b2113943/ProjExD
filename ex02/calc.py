ee85a7273730c93ddc9823a83f63480a9b6dcfce
C0B21139_fuse_ryota <c0b2113943@edu.teu.ac.jp>
2022-10-04 14:57:56 +0900

    練習６

--- ex02/calc.py
+++ ex02/calc.py
@@ -8,7 +8,12 @@ def click(event):  #練習３
     btn = event.widget
     num = int(btn["text"])
     #tkm.showinfo(f"{num}", f"「{num}のボタンが押されました」")
-    entry.insert(tk.END, num)
+    entry.insert(tk.END, num) #5
+
+def click2(event):  #6
+    btn = event.widget
+    num = str(btn["text"])
+    entry.insert(tk.END, num) 
 
 root =tk.Tk()               #練習１
 root.geometry("300x500")
@@ -27,5 +32,9 @@ for i, num in enumerate(range(9,-1, -1), 1):  #練習２
     if i%3 == 0:
         r += 1
         c = 0
+
+btn = tk.Button(root, text="+", font=("", 30), width=4, height=2)
+btn.bind("<1>", click2)
+btn.grid(row=4, column=1)
 
 root.mainloop()
