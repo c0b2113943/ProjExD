98604ebbf08498598ed678d4d211a3d6d40c400c
C0B21139_fuse_ryota <c0b2113943@edu.teu.ac.jp>
2022-10-18 16:18:28 +0900

    4限中②

--- ex03/maze.py
+++ ex03/maze.py
@@ -13,10 +13,10 @@ mod=0
 def key_down(event):
     global key,mod
     key=event.keysym
-    if key=="w":
-            mod=1
+    if key=="w":     #モード切替
+            mod=1   #壁モード
     if key=="n":
-        mod=0  
+        mod=0      #ノーマルモード
     restert()
     if mod==1:
         main_proc2()
@@ -28,54 +28,51 @@ def key_up(event):
     global key
     key=""
 
-def main_proc():
+def main_proc():             #ノーマルモードでの動き（道を進む）
     global key,mx,my,cx,cy
     if key=="Up":
-        if maze_list[my-1][mx]==0:
+        if maze_list[my-1][mx]==0: #動こうとしている先のmaze_list（０と１の行列）を見て道なら現在位置を更新する
             my-=1
             cy-=100
     elif key=="Down":
-        if maze_list[my+1][mx]==0:
+        if maze_list[my+1][mx]==0:#動こうとしている先のmaze_list（０と１の行列）を見て道なら現在位置を更新する
             my+=1
             cy+=100
     elif key=="Left":
-        if maze_list[my][mx-1]==0:
+        if maze_list[my][mx-1]==0:#動こうとしている先のmaze_list（０と１の行列）を見て道なら現在位置を更新する
             mx-=1
             cx-=100
     elif key=="Right":
-        if maze_list[my][mx+1]==0:
+        if maze_list[my][mx+1]==0:#動こうとしている先のmaze_list（０と１の行列）を見て道なら現在位置を更新する
             mx+=1
             cx+=100
     #print(f"{mx}")
     canvas.coords("tori",cx,cy)
 
-def main_proc2():
+def main_proc2():               #壁モードの動き（壁の中を動く。道は進めない）
     global key,mx,my,cx,cy,mod
     if key=="Up":
-        if maze_list[my-1][mx]==1:
+        if maze_list[my-1][mx]==1:#動こうとしている先のmaze_list（０と１の行列）を見て壁なら現在位置を更新する
             my-=1
             cy-=100
     elif key=="Down":
-        if maze_list[my+1][mx]==1:
+        if maze_list[my+1][mx]==1:#動こうとしている先のmaze_list（０と１の行列）を見て壁なら現在位置を更新する
             my+=1
             cy+=100
     elif key=="Left":
-        if maze_list[my][mx-1]==1:
+        if maze_list[my][mx-1]==1:#動こうとしている先のmaze_list（０と１の行列）を見て壁なら現在位置を更新する
             mx-=1
             cx-=100
     elif key=="Right":
-        if maze_list[my][mx+1]==1:
+        if maze_list[my][mx+1]==1:#動こうとしている先のmaze_list（０と１の行列）を見て壁なら現在位置を更新する
             mx+=1
             cx+=100
     canvas.coords("tori",cx,cy)
 
-
-
-
-def restert():
+def restert():                      #Rキーを押したときに再起動する
     global cx ,cy,key,mx,my,maze_list,tori,mod
     if key=="r":
-        maze_list=maze_maker.make_maze(15,9)
+        maze_list=maze_maker.make_maze(15,9)   #新しいmaze_list（０と１の行列）を使い描画し直す
         maze_maker.show_maze(canvas,maze_list) 
         cx=150
         cy=150
@@ -86,43 +83,43 @@ def restert():
             40 ,
             font=("", 20),
             text="[Nキー]ノーマルモード\n[Wキー]壁モード\n[Rキー]再起動")
-        canvas.lift("tori",)
+        canvas.lift("tori",)  #こうかとんを前面に持ってくる
         mod=0
         makea_goll()
 
-def makea_goll():
+def makea_goll():             #ゴールを作成する。道であり三方を壁に囲まれた行き止まりをゴールの候補として得る。
     global maze_list,ans1,ans2
     gool=[]
     for nam,i in enumerate(maze_list):
         for num2,j in enumerate (i):
             if j==0:
-                if i[num2-1]==1 and i[num2+1]==1 and maze_list[nam-1][num2]==1:
+                if i[num2-1]==1 and i[num2+1]==1 and maze_list[nam-1][num2]==1:    #下以外が壁の時
                     gool.append((nam,num2))
-                if i[num2-1]==1 and i[num2+1]==1 and maze_list[nam+1][num2]==1:
+                if i[num2-1]==1 and i[num2+1]==1 and maze_list[nam+1][num2]==1:    #上以外が壁の時
                     gool.append((nam,num2))
-                if i[num2+1]==1 and maze_list[nam-1][num2]==1 and maze_list[nam+1][num2]==1:
+                if i[num2+1]==1 and maze_list[nam-1][num2]==1 and maze_list[nam+1][num2]==1:   #左以外が壁の時
                     gool.append((nam,num2))
-                if  i[num2-1]==1 and maze_list[nam-1][num2]==1 and maze_list[nam+1][num2]==1:
+                if  i[num2-1]==1 and maze_list[nam-1][num2]==1 and maze_list[nam+1][num2]==1:  #右下以外が壁の時
                     gool.append((nam,num2))   
 
-    len1=len(gool)
-    nam=randint(1,len1-1)
+    len1=len(gool)        #ゴールの候補の長さを取る。
+    nam=randint(1,len1-1) #0番はスタートが入ってくることがあるので除きそれ以外の候補から選ぶ
     ans=gool[nam] 
     ans1,ans2=ans
-    canvas.create_rectangle(100, 100, 200,200, fill = 'green',tag="red") 
-    canvas.create_rectangle(ans2*100, ans1*100, ans2*100+100,ans1*100+100 , fill = 'red',tag="red") 
-    canvas.lift("tori","red")   
+    canvas.create_rectangle(100, 100, 200,200, fill = 'green',tag="red")    #スタートを緑にする
+    canvas.create_rectangle(ans2*100, ans1*100, ans2*100+100,ans1*100+100 , fill = 'red',tag="red") #選ばれたゴールの場所を赤くする
+    canvas.lift("tori","red")   #こうかとんを前面に持ってくる
 
-def goal():
+def goal():        #ゴール判定
     global ans1,ans2,mx,my,maze_list
-    if ans1==my and ans2==mx:
-        canvas.create_text(
+    if ans1==my and ans2==mx:       #上で選ばれゴールについたらメッセ時を表示
+        canvas.create_text(        
             1500 // 2,
             900 // 2,
             font=("", 80),
             text="       ゲームクリア！\n[Rキー]で再起動してね"
         )
-        maze_list=1
+        maze_list=2      #全てを壁でも道でも無いものにし、動けなくする
         
 root=tk.Tk()
 root.title("迷えるこうかとん")
@@ -138,7 +135,7 @@ canvas.pack()
 maze_list=maze_maker.make_maze(15,9)
 maze_maker.show_maze(canvas,maze_list)
 
-tori=tk.PhotoImage(file="ex03/fig/"+str(randint(0,9))+".png")
+tori=tk.PhotoImage(file="ex03/fig/"+str(randint(0,9))+".png")   #起動時はランダムなこうかとんが選ばれる
 canvas.create_image(cx,cy,image=tori,tag="tori")
 
 root.bind("<KeyPress>",key_down)
