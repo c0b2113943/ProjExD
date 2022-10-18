203c621d3566c1696f51cce6f35e868445799e20
C0B21139_fuse_ryota <c0b2113943@edu.teu.ac.jp>
2022-10-18 13:40:11 +0900

    5

--- ex03/maze.py
+++ ex03/maze.py
@@ -3,6 +3,10 @@ cx=300
 cy=400
 key=""
 
+def key_down(event):
+    global key
+    key=event.keysym
+    
 root=tk.Tk()
 root.title("迷えるこうかとん")
 canvas=tk.Canvas(
@@ -16,4 +20,7 @@ canvas.pack()
 
 tori=tk.PhotoImage(file="ex03/fig/5.png")
 canvas.create_image(cx,cy,image=tori,tag="tori")
+
+root.bind("<KeyPress>",key_down)
+
 root.mainloop()
